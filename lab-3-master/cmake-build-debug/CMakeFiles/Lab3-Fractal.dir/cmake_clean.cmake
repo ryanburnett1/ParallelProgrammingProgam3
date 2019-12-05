417ce0b0bbc25ff063d9beddac92102e0b7eaa24
ryanburnett1 <35639955+ryanburnett1@users.noreply.github.com>
2019-12-04 20:45:36 -0600

    Add files via upload

---
+++ lab-3-master/cmake-build-debug/CMakeFiles/Lab3-Fractal.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab3-Fractal.dir/main.c.o"
+  "Lab3-Fractal.exe"
+  "Lab3-Fractal.pdb"
+  "libLab3-Fractal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab3-Fractal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
